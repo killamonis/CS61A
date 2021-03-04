4984cacf7d2bfe90ad0b28983b7c376dd5f3560b
Monis Mohiuddin <monmohiuddin@berkeley.edu>
2021-03-03 17:54:24 -0800

    added lab

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,114 @@
+this_file = 'lab06.py'
+def make_adder_inc(n):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2) 
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    increment = 0
+    def adder(i):
+        nonlocal increment
+        ret = n + i + increment
+        increment += 1
+        return ret
+    return adder
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'make_fib', ['List'])
+    True
+    """
+    last_fib, current_fib  = 0, 1
+    def next_fib():
+        nonlocal last_fib, current_fib
+        ret = last_fib
+        last_fib, current_fib = current_fib, current_fib + last_fib
+        return ret
+    return next_fib
+
+
+# Generators
+def naturals():
+    """A generator function that yields the infinite sequence of natural
+    numbers, starting at 1.
+
+    >>> m = naturals()
+    >>> type(m)
+    <class 'generator'>
+    >>> [next(m) for _ in range(10)]
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    """
+    i = 1
+    while True:
+        yield i
+        i += 1
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it scaled by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale(naturals(), 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    for i in it:
+        yield i * multiplier
+
+def hailstone(n):
+    """
+    >>> for num in hailstone(10):
+    ...     print(num)
+    ...
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    yield n
+    if n <= 1:
+        return n
+
+    if n%2 == 0:
+            yield from hailstone(n//2)
+    if n%2:
+            yield from hailstone(n*3 + 1)
+
+        
+     
+
+
---
+++ lab/lab11/lab11.sql
@@ -0,0 +1,19 @@
+.read sp20data.sql
+
+CREATE TABLE obedience AS
+  SELECT seven, instructors from students;
+
+CREATE TABLE smallest_int AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+CREATE TABLE matchmaker AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+-- Ways to stack 4 dogs to a height of at least 170, ordered by total height
+CREATE TABLE stacks_helper(dogs, stack_height, last_height);
+
+-- Add your INSERT INTOs here
+
+
+CREATE TABLE stacks AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
