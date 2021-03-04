27be01f69361d8467c624adb0f81f1c6e47a45f3
Monis Mohiuddin <monmohiuddin@berkeley.edu>
2021-03-03 17:53:06 -0800

    added homework

---
+++ homework/hw09/hw09.sql
@@ -0,0 +1,43 @@
+CREATE TABLE parents AS
+  SELECT "abraham" AS parent, "barack" AS child UNION
+  SELECT "abraham"          , "clinton"         UNION
+  SELECT "delano"           , "herbert"         UNION
+  SELECT "fillmore"         , "abraham"         UNION
+  SELECT "fillmore"         , "delano"          UNION
+  SELECT "fillmore"         , "grover"          UNION
+  SELECT "eisenhower"       , "fillmore";
+
+CREATE TABLE dogs AS
+  SELECT "abraham" AS name, "long" AS fur, 26 AS height UNION
+  SELECT "barack"         , "short"      , 52           UNION
+  SELECT "clinton"        , "long"       , 47           UNION
+  SELECT "delano"         , "long"       , 46           UNION
+  SELECT "eisenhower"     , "short"      , 35           UNION
+  SELECT "fillmore"       , "curly"      , 32           UNION
+  SELECT "grover"         , "short"      , 28           UNION
+  SELECT "herbert"        , "curly"      , 31;
+
+CREATE TABLE sizes AS
+  SELECT "toy" AS size, 24 AS min, 28 AS max UNION
+  SELECT "mini"       , 28       , 35        UNION
+  SELECT "medium"     , 35       , 45        UNION
+  SELECT "standard"   , 45       , 60;
+
+-- copy your solution from prev hw!
+-- The size of each dog
+CREATE TABLE size_of_dogs AS
+  SELECT dogs.name, sizes.size from dogs, sizes
+    WHERE dogs.height > sizes.min and dogs.height <= sizes.max;
+
+-- All dogs with parents ordered by decreasing height of their parent
+CREATE TABLE by_parent_height AS
+  SELECT a.child from parents as a, dogs as b
+  WHERE a.parent = b.name order by -b.height;
+
+-- Filling out this helper table is optional
+
+-- Sentences about siblings that are the same size
+CREATE TABLE sentences AS
+  SELECT a.name || " and " || b.name || " are " || b.size || " siblings"
+    from size_of_dogs as a, size_of_dogs as b, parents as c, parents as d
+    where a.size == b.size and a.name != b.name and c.child = a.name and d.child = b.name and c.parent = d.parent and a.name < b.name;
